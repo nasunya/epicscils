ebf62f4bcd92073a2c4435091133496b9f7613e0
nasunya <tischina-nast2012@yandex.ru>
2019-11-25 16:01:36 +0300

    Menu

--- js/script.js
+++ js/script.js
@@ -19,12 +19,11 @@ const humburger = document.querySelector ('.burger');
 
 humburger.addEventListener('click', function(e){
   burgerMenu.classList.add('main-nav--open')
-})
+});
 
-// const close =   humburger.classList.add('burger--close')
-close.addEventListener('click', function(e){
-
-  burgerMenu.classList.remove('main-nav--open')
+const  burgerClose = humburger.classList.add('burger--close')
+burgerClose.addEventListener('click', function(e){
+burgerMenu.classList.remove('main-nav--open')
 })
 
 // let mobMenu = document.querySelector('.main-nav');
